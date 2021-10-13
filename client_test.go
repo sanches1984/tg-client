8715d16611bf5390038fc1a10b707efb85d1a5a1
Alexander Kononykhin <alexander.kononykhin@corp.mail.ru>
2021-10-13 19:08:51 +0300

    test

--- client_test.go
+++ client_test.go
@@ -1,14 +1,9 @@
 package tg_client
 
 import (
-	"context"
-	"fmt"
 	"testing"
 )
 
 func TestSuccess(t *testing.T) {
-	client, _ := New("1926987692:AAETyEmB8gfLzMdOQnOg8pfT3yymp1kQn4I")
-	client.Listen(context.Background(), func(ctx context.Context, msg IncomingMessage) {
-		fmt.Println(msg.FileURL)
-	})
+
 }
